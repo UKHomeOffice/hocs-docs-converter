3e0d4b617930429c87e79df9f2db3f9e91e01425
Stuart <stuart.whitlam@digital.homeoffice.gov.uk>
2019-02-12 14:08:16 +0000

    Add log events and increase max file size

---
+++ src/main/java/uk/gov/digital/ho/hocs/document/LogEvent.java
@@ -0,0 +1,8 @@
+package uk.gov.digital.ho.hocs.document;
+
+public enum LogEvent {
+    DOCUMENT_CONVERSION_FAILURE,
+    DOCUMENT_CONVERSION_SUCCESS;
+
+    public static final String EVENT = "event_id";
+}
