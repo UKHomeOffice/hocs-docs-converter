884b224f44cfb51f421d76d5992de8deede13062
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-25 23:00:36 +0100

    Tiny code cleanup

--- src/main/java/uk/gov/digital/ho/hocs/document/DocumentConversionResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/DocumentConversionResource.java
@@ -2,18 +2,18 @@ package uk.gov.digital.ho.hocs.document;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.jodconverter.DocumentConverter;
 import org.jodconverter.document.DefaultDocumentFormatRegistry;
 import org.jodconverter.document.DocumentFormat;
 import org.jodconverter.office.OfficeException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -25,32 +25,32 @@ import static uk.gov.digital.ho.hocs.document.LogEvent.*;
 @Slf4j
 public class DocumentConversionResource {
 
+    private static final DocumentFormat outputFormat = DefaultDocumentFormatRegistry.PDF;
     private DocumentConverter converter;
-    private final DocumentFormat outputFormat =  DefaultDocumentFormatRegistry.PDF;
 
     @Autowired
-    public DocumentConversionResource( DocumentConverter converter) {
-        this.converter  = converter;
+    public DocumentConversionResource(DocumentConverter converter) {
+        this.converter = converter;
     }
 
     @PostMapping("/uploadFile")
     public void uploadFile(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws IOException {
 
-            String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
-            if(fileExtension == null) {
-                log.info("Cannot convert document {}, file has no extension", file.getOriginalFilename(), value(EVENT, DOCUMENT_CONVERSION_INVALID_FORMAT));
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.flushBuffer();
-                return;
-            }
+        String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+        if (fileExtension == null) {
+            log.warn("Cannot convert document {}, file has no extension", file.getOriginalFilename(), value(EVENT, DOCUMENT_CONVERSION_INVALID_FORMAT));
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.flushBuffer();
+            return;
+        }
 
-            DocumentFormat format = DefaultDocumentFormatRegistry.getFormatByExtension(fileExtension);
-            if(format == null) {
-                log.info("Cannot convert document {}, unsupported file format", file.getOriginalFilename(), value(EVENT, DOCUMENT_CONVERSION_INVALID_FORMAT));
-                response.setStatus(HttpStatus.BAD_REQUEST.value());
-                response.flushBuffer();
-                return;
-            }
+        DocumentFormat format = DefaultDocumentFormatRegistry.getFormatByExtension(fileExtension);
+        if (format == null) {
+            log.warn("Cannot convert document {}, unsupported file format", file.getOriginalFilename(), value(EVENT, DOCUMENT_CONVERSION_INVALID_FORMAT));
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.flushBuffer();
+            return;
+        }
 
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             converter
@@ -67,8 +67,8 @@ public class DocumentConversionResource {
             response.flushBuffer();
 
         } catch (OfficeException | IOException e) {
-                log.error("Error converting document {}", e.getMessage(), value(EVENT, DOCUMENT_CONVERSION_FAILURE), value(EXCEPTION, e.toString()));
-                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            log.error("Error converting document {}", e.getMessage(), value(EVENT, DOCUMENT_CONVERSION_FAILURE), value(EXCEPTION, e.toString()));
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
 
     }
@@ -77,9 +77,9 @@ public class DocumentConversionResource {
 
         response.setContentType(outputFormat.getMediaType());
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-            "attachment; filename="
-                    + FilenameUtils.getBaseName(filename)
-                    + "."
-                    + outputFormat.getExtension());
+                "attachment; filename="
+                        + FilenameUtils.getBaseName(filename)
+                        + "."
+                        + outputFormat.getExtension());
     }
 }
--- src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsConversionApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsConversionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HocsDocsConversionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HocsDocsConversionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HocsDocsConversionApplication.class, args);
+    }
 }
