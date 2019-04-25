f22e6f1ed8eda11dd475b3def4eafe2da876875d
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-25 19:45:37 +0100

    Move to Java 11

--- src/main/java/uk/gov/digital/ho/hocs/document/DocumentConversionResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/DocumentConversionResource.java
@@ -5,7 +5,6 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
