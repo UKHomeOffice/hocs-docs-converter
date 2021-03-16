76d923839068dc8bc337f37e5d8965b961e2306a
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-03-16 10:09:17 +0000

    HOCS-2637: refactor deploy file (#21)
    Currently we have a if/else tower that handles the setting of the
    KUBE_TOKEN environment variable. As we are now passing this
    in directly this is no longer needed. Alongside this, we now set the
    pipeline to fail if it returns a none 0 status code from a command.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
 export KUBE_NAMESPACE=${ENVIRONMENT}
 export KUBE_SERVER=${KUBE_SERVER}
+export KUBE_TOKEN=${KUBE_TOKEN}
 
 if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
@@ -14,42 +16,6 @@ then
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="10"
-fi
-
-if [[ ${KUBE_NAMESPACE} == "cs-prod" ]] ; then
-    echo "deploy ${VERSION} to prod namespace, using HOCS_DOCS_CONVERTER_CS_PROD drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_CS_PROD}
-elif [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
-    echo "deploy ${VERSION} to prod namespace, using HOCS_DOCS_CONVERTER_WCS_PROD drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_WCS_PROD}
-elif [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
-    echo "deploying ${VERSION} to test namespace, using HOCS_DOCS_CONVERTER_CS_QA drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_CS_QA}
-elif [[ ${KUBE_NAMESPACE} == "wcs-qa" ]] ; then
-    echo "deploying ${VERSION} to test namespace, using HOCS_DOCS_CONVERTER_WCS_QA drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_WCS_QA}
-elif [[ ${KUBE_NAMESPACE} == "cs-demo" ]] ; then
-    echo "deploy ${VERSION} to demo namespace, using HOCS_DOCS_CONVERTER_CS_DEMO drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_CS_DEMO}
-elif [[ ${KUBE_NAMESPACE} == "wcs-demo" ]] ; then
-    echo "deploy ${VERSION} to demo namespace, using HOCS_DOCS_CONVERTER_WCS_DEMO drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_WCS_DEMO}
-elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
-    echo "deploy ${VERSION} to dev namespace, using HOCS_DOCS_CONVERTER_CS_DEV drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_CS_DEV}
-elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
-    echo "deploy ${VERSION} to dev namespace, using HOCS_DOCS_CONVERTER_WCS_DEV drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_WCS_DEV}
-elif [[ ${KUBE_NAMESPACE} == "hocs-qax" ]] ; then
-    echo "deploy ${VERSION} to qax namespace, using HOCS_DOCS_CONVERTER_QAX drone secret"
-    export KUBE_TOKEN=${HOCS_DOCS_CONVERTER_QAX}
-else
-    echo "Unable to find environment: ${ENVIRONMENT}"
-fi
-
-if [[ -z ${KUBE_TOKEN} ]] ; then
-    echo "Failed to find a value for KUBE_TOKEN - exiting"
-    exit -1
 fi
 
 cd kd
