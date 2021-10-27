c7199007d87ecdd4876d6850b0722bcf48b41838
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-10-27 12:23:45 +0100

    * Add Readiness probe back * Shutdown hook logging * Add pre-stop sleep

--- src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsConversionApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsConversionApplication.java
@@ -1,8 +1,12 @@
 package uk.gov.digital.ho.hocs.document;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
+@Slf4j
 @SpringBootApplication
 public class HocsDocsConversionApplication {
 
@@ -10,4 +14,9 @@ public class HocsDocsConversionApplication {
 		SpringApplication.run(HocsDocsConversionApplication.class, args);
 	}
 
+	@PreDestroy
+	public void stop() {
+		log.info("hocs-docs-converter stopping gracefully");
+		log.info("Stopping gracefully");
+	}
 }
